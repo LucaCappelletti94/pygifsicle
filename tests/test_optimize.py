ab1749290c2eeba152c48e95bbba7469869e052d
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-05-08 16:49:55 -0400

    Update test_optimize.py

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -1,7 +1,17 @@
 from pygifsicle import optimize
 import os
+from pathlib import Path
 
 def test_optimize():
     optimize("tests/big.gif", "small.gif")
     optimize("small.gif")
     os.remove("small.gif")
+    
+    
+def test_optimize_pathlib():
+    here = Path(__file__).parent.resolve()
+    source = here / "big.gif"
+    destination = here / "small.gif"
+    optimize(source, destination)
+    optimize(destination)
+    os.remove(destination)
